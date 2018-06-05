8179d7c70332fd96390722e394dfa920a41fa164
hcjun94 <hcjun94@hotmail.com>
2018-06-05 13:33:21 -0500

    added compareVer method. Solution Provided.

--- src/versionComparison.java
+++ src/versionComparison.java
@@ -1,23 +1,34 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class versionComparison {
     public static void main(String[] args) {
-        String firstNum;
-        String fNumArr[];
+        String firstNum, secondNum;
+
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Input the first version Number to compare: ");
+        System.out.print("Input the first version Number : ");
         firstNum = sc.nextLine();
+        System.out.print("Input the second version Number : ");
+        secondNum = sc.nextLine();
 
+        compareVer(firstNum, secondNum);
 
+    }
 
-        System.out.println(firstNum);
+    private static void compareVer(String ver1, String ver2) {
+        String[] ver1_array = ver1.trim().split("[.]");
+        String[] ver2_array = ver2.trim().split("[.]");
 
-        fNumArr = firstNum.split("\\.");
-
-
-
+        String result;
+        for (int i = 0; i < Math.min(ver1_array.length, ver2_array.length); i++) {
+            if (Integer.parseInt(ver1_array[i]) > Integer.parseInt(ver2_array[i])) {
+                result = ver1 + " > " + ver2;
+                System.out.println(result);
+                return;
+            }
+        }
+        result = ver1 + " < " + ver2;
+        System.out.println(result);
 
     }
 }
